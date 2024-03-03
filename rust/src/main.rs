4649474122594f792bb97b322ee48d676cc7192e
Robert Kennedy <robert076kennedy@gmail.com>
2024-03-02 19:59:13 -0500

    Implemented cargo clippy suggestions saving 4 secs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -14,10 +14,10 @@ struct Station {
 
 fn main() {    
     let mut stations: HashMap<String, Station> = HashMap::new();
-    let lines = BufReader::with_capacity(BUFFER_SIZE, File::open("../measurements.data").unwrap()).lines().flatten();
+    let lines = BufReader::with_capacity(BUFFER_SIZE, File::open("../measurements.data").unwrap()).lines().map_while(Result::ok);
 
     for line in lines {
-        let mut line_split = line.split(";");
+        let mut line_split = line.split(';');
         let station_name = line_split.next().unwrap().to_string();
         let measurement: f32 = line_split.next().unwrap().parse().unwrap();
         match stations.get_mut(&station_name) {
@@ -46,14 +46,11 @@ fn main() {
     let mut station_names = stations.keys().collect::<Vec<&String>>();
     station_names.sort();
     for (station_name, should_add_comma) in station_names.iter().enumerate().map(|(i, s)| (*s, i != station_names.len() - 1)) {
-        match stations.get(station_name) {
-            Some(station) => {
-                print!("{}={:.1?}/{:.1?}/{:.1?}", station_name, station.min, station.sum / station.n as f32, station.max);
-                if should_add_comma {
-                    print!(", ")
-                }
-            },
-            None => {}
+        if let Some(station) = stations.get(station_name) {
+            print!("{}={:.1?}/{:.1?}/{:.1?}", station_name, station.min, station.sum / station.n as f32, station.max);
+            if should_add_comma {
+                print!(", ")
+            }
         }
     }
     print!("}}");
